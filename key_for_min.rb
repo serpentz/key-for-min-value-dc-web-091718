35fb3019e8e9f0cbf288d587be85c951442c018b
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-05 16:08:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 if !name_hash.empty
   key_for_min_value = name_hash.keys[0]
-  name_hash.collect {|key, value|
+  name_hash.each {|key, value|
     key_for_min_value = value > name_hash[key_for_min_value] ? key : key_for_min_value
   }
 end
